e5eeeb2b1df3805acf3de95a203ead4e0ba2079a
Shivam Dubey <72991053+shivampycharm@users.noreply.github.com>
2023-01-11 23:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+	// convert the string to lowercase and remove non-alphanumeric characters
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // check if the string is equal to its reverse
+    return str === str.split("").reverse().join("");
 }
 module.exports = palindrome
